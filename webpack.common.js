9bca794231adc5a52b330e670aa7a5a1a431d358
Kyle Nel <22053773+kdevnel@users.noreply.github.com>
2021-06-19 17:18:50 +0200

    Updated build to use /docs

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   output: {
     filename: "app.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     clean: true,
   },
   module: {
